dec346a7fd4c029f55af90faa94f053f6be3c732
kimgsun <gsune@kakao.com>
2025-08-06 15:14:18 +0900

    20250806 JDBC Example

---
+++ src/main/java/org/example/이론/JAVA_DB.java
@@ -0,0 +1,19 @@
+package org.example.이론;
+
+/*
+    === JDBC VS JPA VS MyBatis ===
+    : Java(Spring Boot)에서 DB와 연동하기 위한 방법
+
+    1) JDBC (JDBC API 기반)
+        : Java에서 DB에 접근할 수 있도록 제공하는 표준 API
+        - SQL문을 문자열로 작성
+        >> Connection, PreparedStatement, ResultSet 등의 객체를 통해 직접 DB와 통신
+
+        cf) Spring JDBC - 기존의 JDBC를 간소화하는 JDBC 추상화 라이브러리 (스프링 제공)
+
+    2) JPA (ORM, Object Relational Mapping 기반)
+
+ */
+
+public class JAVA_DB {
+}
